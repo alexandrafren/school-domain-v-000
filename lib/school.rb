b10c9b244a740ae6a4f4f406c170f36426c2b47c
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-12 22:28:03 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student(name, grade)
+    if !roster[grade].include?(grade)
+    @roster[grade] = []
+    @roster[grade] << name
+  end
+end
