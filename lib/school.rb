4b27f4940c160ff94b3f1d9ca656b7c01f5307ec
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-12 22:32:19 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,7 @@ class School
   def add_student(name, grade)
     if !@roster.include?(grade)
       @roster[grade] = []
-    else
-      @roster[grade] << name
     end
+      @roster[grade] << name
   end
 end
